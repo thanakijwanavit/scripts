3fff014a1de82a8a417e39f5c4991e38ab0d3b8f
nic <nwanavit@gmail.com>
2017-12-14 18:25:24 +0000

    add some startup scripts

---
+++ gitconfig.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ sudo apt-get install git
+ git config --global user.email "nwanavit@gmail.com"
+ git config --global user.name "nic"
---
+++ startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#sudo useradd nic
+#sudo passwd nic
+#sudo usermod -aG sudo nic
+#su nic
+sudo ./gitconfig.sh
+sudo apt-get install tmux
+sudo ./putallincommandlist.sh
+
