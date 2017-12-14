12d53405ec528a1b39b8bf1970459de607e0ac99
thanakijwanavit <nwanavit@gmail.com>
2017-12-15 00:00:26 +0700

    Update connectssh.sh

--- connectssh.sh
+++ connectssh.sh
@@ -40,4 +40,4 @@ shift $((OPTIND - 1))
 
 
 
-tmux new-session -s amazon -d 'ssh -R $port:localhost:22 -i "nicwanavit.pem" ec2-user@$ip; return'
+tmux new-session -s amazon -d 'cd /home/nnic/scripts/ ;ssh -R $port:localhost:22 -i "nicwanavit.pem" ec2-user@$ip ;read'
