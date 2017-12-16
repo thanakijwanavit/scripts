ffaf9caf3c68968acc35e886785c536cc307e406
nic <nwanavit@gmail.com>
2017-12-16 23:51:35 +0700

    add update goalng tmux python and thefuck
    Former-commit-id: 512142935f7cf1212a2545dc184883790c498568

--- putallincommandlist.sh
+++ putallincommandlist.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install golang-go
+sudo apt-get install tmux
+sudo apt-get install python3-dev python3 pip
+sudo pip3 install thefuck
 
 ./putincommandlist.sh findip.sh findip
 ./putincommandlist.sh connectssh.sh connectssh
