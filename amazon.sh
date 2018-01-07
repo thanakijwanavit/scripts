fa6a7303880aede6b96928d619324fc500394c39
Ubuntu <ubuntu@ip-172-31-32-178.us-west-2.compute.internal>
2018-01-07 11:08:17 +0000

    automatically find key
    Former-commit-id: cea1ab18eec8a4117cf1fdca16a705e008deb44a

--- amazon.sh
+++ amazon.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-ssh -i "/home/$USER/scripts/amazonfree.pem" ubuntu@ec2-54-70-87-233.us-west-2.compute.amazonaws.com
+
+keyfile=$(locate amazonfree.pem)
+
+echo $keyfile is used as the  keyfile
+
+ssh -i $keyfile ubuntu@ec2-54-70-87-233.us-west-2.compute.amazonaws.com
+#/home/$USER/scripts/amazonfree.pem
