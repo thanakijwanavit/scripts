461aedcf26e9643b2290c62fe366fff7558b1c3c
nic <nwanavit@gmail.com>
2018-01-21 18:46:12 -0500

    add some browser sites
    Former-commit-id: 6dfb15f235e747396c5649ad604ce64275e527b1

--- amazon.sh
+++ amazon.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-keyfile=$(locate amazonfree.pem)
+package='locate'
 
-echo $keyfile is used as the  keyfile
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $package|grep 'install ok installed')
+echo Checking for locate: $PKG_OK
+if [ "" == "$PKG_OK" ]; then
+	echo 'installing' $package
+	sudo apt-get --force-yes --yes install $package
+fi
+
+if [ $package == 'locate' ]; then
+	keyfile=$(locate amazonfree.pem)
+	echo locate is used
+fi
+
+if [ $package == 'find' ]; then
+	keyfile=$(find $HOME -name 'amazonfree.pem')
+	echo find is used
+fi	
+echo $keyfile is used as the keyfile
 
 ssh -i $keyfile ubuntu@ec2-54-70-87-233.us-west-2.compute.amazonaws.com
 #/home/$USER/scripts/amazonfree.pem
