4f529982a950349116ce1267b04a75fa9ec88d61
Thanakij Wanavit <nic@nics-MacBook-3.local>
2018-01-26 14:25:32 +0700

    add flask
    Former-commit-id: 8e0831fd3f490d826d8d29d500c9503aa51d51c8

---
+++ flask/hello.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from flask import Flask
+import turnonlight, requests
+
+app = Flask(__name__)
+rootpage = '''
+<form action="/action_page.php">
+  First name:<br>
+  <input type="text" name="firstname" value="Mickey"><br>
+  Last name:<br>
+  <input type="text" name="lastname" value="Mouse"><br><br>
+  <input type="submit" value="Submit">
+</form>
+'''
+
+
+
+
+
+@app.route("/")
+def hello():
+	return rootpage
+
+@app.route("/light")
+	return 'light is on'
+
+@app.route("/lightoff")
+def lightoff()
+	turnonlight.off()
+	return 'light is off'
+
+@app.route("/lighton")
+def light():
+	turnonlight.on()
+	return 'light is on'
+
