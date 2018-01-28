499a5a1bd0ee5e1aeb8edf837943724287eb9a03
Ubuntu <ubuntu@ip-172-31-32-178.us-west-2.compute.internal>
2018-01-28 13:44:42 +0000

    add fowardport.sh
    Former-commit-id: 89616ab0097bc048ef7935177209b5b151c6eaac

--- fowardport.sh
+++ fowardport.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
-
-package=autossh
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $package|grep 'install ok install')
-echo Checking for locate: $PKG_OK
-if [ "" == "$PKG_OK" ]; then
-        echo 'installing' $package
-        sudo apt-get --force-yes --yes install $package
-fi
-
 usage="$(basename "$0") [-h] [-s n] -- program to calculate the answer to life, the universe and everything
 
 where:
     -h  show this help text
     -s  set the seed value (default: 42)
-    -p set port (default 10000)
+    -p set port to (default 10000)
     -i amazon ip (default: 172.31.32.178)
     -c connect
+    -f port from (default:80)
 "
+from=80
 seed=42
-port=10000
+port=23456
 ip='ec2-54-70-87-233.us-west-2.compute.amazonaws.com'
-while getopts ':hs:p:i:c' option; do
+while getopts ':hs:p:i:cf:' option; do
   case "$option" in
-    c) tmux new-session -s amazon -d "cd /home/nic/scripts/ ;autossh -R $port:localhost:22 -i '/home/nic/scripts/amazonfree.pem' ubuntu@$ip ;read"
+    c) tmux new-session -s amazon -d "cd /home/nic/scripts/ ;ssh -R $port:localhost:22 -i '/home/nic/scripts/amazonfree.pem' nic@$ip ;read"
        ;;
     h) echo "$usage"
        exit
@@ -32,6 +25,8 @@ while getopts ':hs:p:i:c' option; do
     p) port=$OPTARG
        ;;
     i) ip=$OPTARG
+       ;;
+    f) from=$OPTARG
        ;;
     :) printf "missing argument for -%s\n" "$OPTARG" >&2
        echo "$usage" >&2
@@ -49,6 +44,6 @@ shift $((OPTIND - 1))
 
 
 
-tmux new-session -s amazon -d "cd /home/nic/scripts/ ;autossh -R $port:localhost:22 -i 'amazonfree.pem' ubuntu@$ip ;read"
+tmux new-session -s amazon -d "cd /home/nic/scripts/ ;ssh -R $port:localhost:$from -i 'amazonfree.pem' nic@$ip ;read"
 
 
